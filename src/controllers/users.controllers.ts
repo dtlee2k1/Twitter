3874d072de03b456fa1d15794395c9431efd3292
Le Duc Thai <leducthai2001@gmail.com>
2023-11-14 17:58:48 +0700

    create Users schema

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from 'express'
+import userService from '~/services/users.services'
 
 // Controller có tác vụ xử lý logic kết quả trả về
-export const usersController = (req: Request, res: Response) => {
+export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
   // Thực hiện xử lý với dữ liệu
   if (email === 'leducthai2001@gmail.com' && password === '123456') {
     // Trả về phản hồi cho client
     return res.json({
-      message: 'Login successful'
+      message: 'Login successfully'
     })
   }
   // Trả về phản hồi cho client
-  return res.status(400).json({ error: 'email or password incorrect' })
+  return res.status(400).json({ message: 'email or password incorrect' })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  // Thực hiện xử lý với dữ liệu
+  try {
+    const result = await userService.register({ email, password })
+    // Trả về phản hồi cho client
+    res.json({
+      message: 'Register successfully',
+      result
+    })
+  } catch (error) {
+    return res.status(400).json({ message: 'Register failed' })
+  }
 }
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,11 +1,13 @@
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import { Collection, Db, MongoClient, ServerApiVersion } from 'mongodb'
 import 'dotenv/config'
+import User from '~/models/schemas/User.schema'
 
-const { DB_USERNAME, DB_PASSWORD } = process.env
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_USERS_COLLECTION } = process.env
 const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@twitter.7erkx9h.mongodb.net/?retryWrites=true&w=majority`
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
 
   constructor() {
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,19 +18,22 @@ class DatabaseService {
         deprecationErrors: true
       }
     })
+    this.db = this.client.db(DB_NAME)
   }
 
-  public async connect() {
+  async connect() {
     try {
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
     } catch (error) {
-      console.dir(error)
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
+      console.log('Error:', error)
+      throw error
     }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(DB_USERS_COLLECTION as string)
   }
 }
 
